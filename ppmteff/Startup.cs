19ef3f87dbeedcc3bcc4f91e35e99aa5043e814b
Tefferson <teffersonguterres@gmail.com>
2016-04-13 23:03:22 -0300

    initial commit

---
+++ ppmteff/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ppmteff.Startup))]
+namespace ppmteff
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
